4da039281a693d544bfebb042e235e406049236b
bagus priambudi <baguspriambudi77@gmail.com>
2020-09-14 18:04:06 +0700

    delete auth isUser

--- midlleware/auth.js
+++ midlleware/auth.js
@@ -25,23 +25,3 @@ exports.isAdmin = async (req, res, next) => {
     }
 }
 
-exports.isUser = async (req, res, next) => {
-    try {
-        const headers = req.headers.authorization
-        if (!headers) {
-            return http_error.http_responError(res, 'please provide token')
-        }
-
-        const token = headers.split(' ')[1]
-        const decode = JWT.verify(token, JWTsekret)
-        req.user = decode
-
-        const find = await User.findById({ _id: req.user._id })
-        if (!find || req.user.role_id != 'user') {
-            return http_error.http_responError_notfound(res, 'User is not acces')
-        }
-        next()
-    } catch (error) {
-        return http_error.http_responValidation(res, 'validation error', error.message)
-    }
-}
